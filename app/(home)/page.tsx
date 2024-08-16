5d203cb20152e6979d1b26f42724b7725ce1db42
pyoja <wjdgns13nr@naver.com>
2024-08-16 17:10:22 +0900

    server side1

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,16 @@
-"use client";
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: "Home",
+};
 
-// export const metadata = {
-//   title: "Home",
-// };
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
+async function getMovies() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
