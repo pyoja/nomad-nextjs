c1dd46745a74237cc7dd84223be8ed375d1fab18
pyoja <wjdgns13nr@naver.com>
2024-08-16 15:29:35 +0900

    metadata

--- app/about-us/layout.tsx
+++ app/about-us/layout.tsx
@@ -1,3 +1,7 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function AboutUsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <div>{children}&copy; Next is great!</div>;
 }
