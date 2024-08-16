56e9f067ac2490534afd00380c196ae2b13e4848
pyoja <wjdgns13nr@naver.com>
2024-08-16 13:47:40 +0900

    not found

---
+++ components/navigation.tsx
@@ -0,0 +1,10 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+            </ul>
+        </nav>
+    )
+}
