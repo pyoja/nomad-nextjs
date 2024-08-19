3da04ed146ad8bb2f1acd59595b1eccf59eab028
pyoja <wjdgns13nr@naver.com>
2024-08-19 16:05:45 +0900

    movie styles ( router )

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,8 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // return response.json();
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("error..server..");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
